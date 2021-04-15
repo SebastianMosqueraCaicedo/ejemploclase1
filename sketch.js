9c0cfbf156d028ad1e436aed22395be3a6acaa7c
SebastianMosqueraCaicedo <1005893565@u.icesi.edu.co>
2021-04-14 21:38:43 -0500

    :wq
    ad

--- sketch.js
+++ sketch.js
@@ -4,5 +4,16 @@ function setup() {
 
 function draw() {
   background(220);
-  text("Ejercisio Git",50,50)
+  fill(0)
+  text("Ejercisio Git",50 + (frameCount),50)
+  text("Hola",-400 + (frameCount),50)
+  for (let index = 0; index < frameCount; index+=random(2,10)) {
+    for (let j = 0; j < frameCount; j+=random(2,10)) {
+      fill(random(100,200))
+    text("Algo mas",60 +(index*2),60 +(index*2) + j)
+
+    }
+    
+    
+  }
 }
